737267ce47f56da2721f81ad9d54c268c25b1d45
Nguyễn Khắc Vũ <chodiem.io@gmail.com>
2022-07-26 23:23:07 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nguyen Khac Vu on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
